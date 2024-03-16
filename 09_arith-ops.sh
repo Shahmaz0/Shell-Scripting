b0e70d792c1baeeae749f31778b4d4bf55d5fb9c
Shahma Ansari <shahmaansari@Shahmas-MacBook-Air.local>
2024-03-16 18:01:29 +0530

    Added: 09_arith-ops.sh

---
+++ 09_arith-ops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Maths Calculation
+
+x=10
+y=2
+
+let mul=$x*$y
+echo "Mul : $mul"
+
+let sum=$x+$y
+echo "Sum : $sum"
+
+let div=$x/$y
+echo "div : $div"
+
+echo "Subtractrion is : $(($x-$y))"
